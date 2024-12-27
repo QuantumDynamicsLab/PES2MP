1880d5776fdb90c8c224a29d9b6e62bf791ce1e4
Apoorv Kushwaha <KUSHWAHA.APOORV@GMAIL.COM>
2024-12-27 13:08:49 +0530

    files+1

--- input_files/aux_scripts/PES_extract/result_extract_Molpro.py
+++ input_files/aux_scripts/PES_extract/result_extract_Molpro.py
@@ -5,12 +5,14 @@ import os
 
 path = os.getcwd()
 
-ct = 1
+##############################################################
 f1 = open("PES.dat" ,"w+")
 f2 = open("PES_err.dat" ,"w+")
 num_files = 6371
+##############################################################
 
-for i in range (1,num_files,1):
+ct = 1
+for i in range (1,num_files+1,1):
     if (os.path.isfile("%d.out" %(i))):
         f= open("%d.out" %(i),"r")
         pattern=re.compile("   R   ")
@@ -19,22 +21,16 @@ for i in range (1,num_files,1):
             for line in f:
                 for match in re.finditer(pattern,line):
                     print("first pattern found!")
-                    f1.write(line)   
-                    f1.write(next(f))   
+                    f1.write(line)
+                    f1.write(next(f))
                     ct = 2
             f.close()
         else:
             for line in f:
                 for match in re.finditer(pattern,line):
-                    f1.write(next(f))   
+                    f1.write(next(f))
             f.close()
-    else: 
-        f2.write("%d.out does not exist" %(i) ) 
+    else:
+        f2.write("%d.out does not exist" %(i) )
 f1.close()
-f2.close()   
-
-
-
-
-
-
+f2.close()
