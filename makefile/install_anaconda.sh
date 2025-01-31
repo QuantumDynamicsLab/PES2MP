a76ca530020fdca4ac54c369beebfb0f0b31d963
Apoorv Kushwaha <KUSHWAHA.APOORV@GMAIL.COM>
2025-01-31 11:16:45 +0530

    Create install_anaconda.sh

---
+++ makefile/install_anaconda.sh
@@ -0,0 +1,11 @@
+# installing anaconda
+
+# download anaconda installer file from website
+# change this based on latest conda release (find on anaconda website linux installation)
+wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+
+# installation
+bash Anaconda3-2024.10-1-Linux-x86_64.sh
+apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+# run the next command in new terminal to enable conda automatically (optional: can be set during conda installation)
+# conda config --set auto_activate_base True
