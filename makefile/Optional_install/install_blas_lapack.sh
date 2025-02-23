7f46ef9c24118cb340fca978058ca9557b04257c
Apoorv Kushwaha <KUSHWAHA.APOORV@GMAIL.COM>
2025-02-23 18:43:04 +0530

    Delete makefile directory

--- makefile/Optional_install/install_blas_lapack.sh
+++
@@ -1,6 +0,0 @@
-# installing gfortran and blas-lapack for MOLSCAT (OPTIONAL)
-
-sudo apt-get update
-sudo apt-get install -y gfortran
-sudo apt-get install -y libblas-dev 
-sudo apt-get install -y liblapack-dev
--- makefile/Optional_install/install_psi4.sh
+++
@@ -1,6 +0,0 @@
-# installing psi4
-
-sudo apt update
-sudo apt install -y psi4  
-
-
