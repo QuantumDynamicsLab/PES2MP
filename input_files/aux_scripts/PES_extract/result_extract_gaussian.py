d3feec35371d017a087a6447d5c23eb5b55e2583
Apoorv Kushwaha <KUSHWAHA.APOORV@GMAIL.COM>
2025-05-14 11:23:51 +0530

    Update result_extract_gaussian.py

--- input_files/aux_scripts/PES_extract/result_extract_gaussian.py
+++ input_files/aux_scripts/PES_extract/result_extract_gaussian.py
@@ -6,12 +6,12 @@ import os
 path = os.getcwd()
 
 ##############################################################
-PES_folder = path + '/Molpro_CP/'
+PES_folder = path + '/Gaussian_CP/'
 
-f1 = open(os.path.join(path, "PES.dat"), "w+")
+f1 = open(os.path.join(path, "PES_raw.dat"), "w+")
 f2 = open(os.path.join(path, "PES_err.dat"), "w+")
 
-num_files = 6371
+num_files = 1424
 ##############################################################
 
 # Change to the user-specified directory
@@ -20,17 +20,19 @@ if os.path.isdir(PES_folder):
 
 
 ct = 1
-pattern1=re.compile("R = ")
-pattern2 = re.compile("sum of fragments =")
+pattern1=re.compile(" R = ")
+pattern2 = re.compile("Counterpoise corrected energy")
 
-for i in range (1,num_files+1,1):
-    if (os.path.isfile("%d.out" %(i))):
-        f= open("%d.out" %(i),"r")
-        print("first file opened!")
+for i in range (0,num_files+1,1):
+    if (os.path.isfile("%d.log" %(i))):
+        f= open("%d.log" %(i),"r")
+        #print("first file opened!")
         for line in f:
             for match in re.finditer(pattern1,line):
-                print("first pattern found!")
+                #print("first pattern found!")
                 f1.write(line)
+        f.close()
+        f= open("%d.log" %(i),"r")
         for line in f:
             for match in re.finditer(pattern2,line):
                 f1.write(line)
@@ -39,3 +41,18 @@ for i in range (1,num_files+1,1):
         f2.write("%d.out does not exist" %(i) )
 f1.close()
 f2.close()
+if os.path.isdir(path):
+    os.chdir(path)
+
+with open("PES_raw.dat") as f1, open("PES.dat", "w") as f3:
+    buffer = ""
+    for line in f1:
+        line = line.strip()
+        if line.startswith("R ="):
+            parts = line.replace("R =", "").replace("Theta=", "").split(",")
+            buffer = f"{parts[0].strip()}, {parts[1].strip()}"
+        elif "Counterpoise corrected energy" in line:
+            energy = line.split("=")[-1].strip()
+            f3.write(f"{buffer}, {energy}\n")
+f3.close()
+
